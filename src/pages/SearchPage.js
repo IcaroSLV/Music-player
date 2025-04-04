5e634ec7f961f9f92a5a5c1bd22f7adb27d57765
unknown <icaroisl6582@gmail.com>
2025-04-04 13:21:42 -0300

    Fix Bug

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -39,6 +39,7 @@ function SearchPage() {
   }, [])
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     const searchArtist = async () => {
       setArtists('')
 
